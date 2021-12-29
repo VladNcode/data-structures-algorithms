9fe977bcf7f05fcf6ff20b9b8bf47bb3991be40a
VladNCode <vldsmrfow@gmail.com>
2021-12-29 12:16:47 +0200

    tested solutions

--- cq.js
+++ cq.js
@@ -7,19 +7,36 @@ const array4 = ['z', 'y', 'x'];
 const array5 = [];
 const array6 = [];
 
-const findCommonItems = (array1, array2) => {
-  const filteredArray = [...new Set([...array1, ...array2])];
+const arr1 = ['a', 'b', 'c', 'e'];
+const arr2 = ['d', 'e', 'f', 'j'];
+
+// const findCommonItems = (array1, array2) => {
+//   const filteredArray = [...new Set([...array1, ...array2])];
+//   const obj = {};
+
+//   filteredArray.forEach(el => (obj[el] = ++obj[el] || 1));
+
+//   for (v in obj) {
+//     if (obj[v] > 1) {
+//       return true;
+//     }
+//   }
+
+//   return false;
+// };
+
+const findCommonItems = (arr1, arr2) => {
   const obj = {};
-
-  filteredArray.forEach(el => (obj[el] = ++obj[el] || 1));
-
-  for (v in obj) {
-    if (obj[v] > 1) {
-      return true;
-    }
-  }
-
-  return false;
+  arr1.forEach(el => (obj[el] = true));
+  return arr2.find(el => obj[el] === true) ? true : false;
 };
 
-console.log(findCommonItems(array5, array6));
+console.log(findCommonItems(arr1, arr2));
+
+const findCommonItems = (arr1, arr2) => {
+  const obj = {};
+  arr1.forEach(el => (obj[el] = true));
+  return arr2.some(el => obj[el] === true);
+};
+
+console.log(findCommonItems(arr1, arr2));
