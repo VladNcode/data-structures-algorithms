56d41002b855f9cb109878af9d1136ab74bc3e28
VladNCode <vldsmrfow@gmail.com>
2021-12-31 07:53:20 +0200

    solved few coding challenges

--- gi(findPairsEqSum).js
+++ gi(findPairsEqSum).js
@@ -63,4 +63,98 @@ const twoItemsEqSumIndexes = function (arr, sum) {
   }
 };
 
-console.log(twoItemsEqSumIndexes([3, 2, 3], 6));
+const twoItemsEqSumIndexes2 = function (arr, sum) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === sum) return [i, j];
+    }
+  }
+};
+
+// console.log(twoItemsEqSumIndexes2([1, 2, 3, 4, 5, 6, 15, 23], 24));
+
+const maxSubArray = function (nums) {
+  let max = -Infinity;
+  let temp = 0;
+  for (let i = 0; i < nums.length; i++) {
+    temp = Math.max(nums[i], nums[i] + temp);
+    // console.log(temp);
+    max = temp > max ? temp : max;
+    console.log(max);
+  }
+  return max;
+};
+
+// console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+
+// const moveZeroes = function (arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === 0) {
+//       arr.splice(arr.indexOf(arr[i]), 1);
+//       arr.push(0);
+//     }
+//   }
+// };
+
+const moveZeroes = nums => {
+  let i = 0,
+    j = 0;
+  while (i < nums.length) {
+    if (nums[i] != 0) {
+      let temp = nums[i];
+      nums[i] = 0;
+      nums[j] = temp;
+      j++;
+    }
+    i++;
+  }
+};
+
+// console.log(moveZeroes([0, 1, 0, 3, 12]));
+
+// const containsDuplicate = function (nums) {
+//   const obj = {};
+//   nums.forEach(num => (obj[num] = ++obj[num] || 1));
+//   for (val in obj) {
+//     if (obj[val] > 1) return true;
+//   }
+
+//   return false;
+// };
+
+const containsDuplicate = function (nums) {
+  const set = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) return true;
+    set.add(nums[i]);
+  }
+  return false;
+};
+
+const containsDuplicate = function (nums) {
+  const obj = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] in obj) return true;
+    obj[nums[i]] = i;
+  }
+
+  return false;
+};
+
+const containsDuplicate = function (nums) {
+  let obj = {};
+
+  for (let num of nums) {
+    if (obj[num]) {
+      return true;
+    } else {
+      obj[num] = 1;
+    }
+  }
+  return false;
+};
+
+const containsDuplicate = nums => [...new Set(nums)].length !== nums.length;
+
+// console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
+// console.log(containsDuplicate([1, 2, 3, 4, 5]));
