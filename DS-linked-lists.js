cc6b03a51966e354e366020d5bd80eeca7e3f8c0
VladNCode <vldsmrfow@gmail.com>
2022-01-01 11:25:14 +0200

    started learning linked lists

---
+++ DS-linked-lists.js
@@ -0,0 +1,21 @@
+// let myLinkedList = {
+//   head: {
+//     value: 10,
+//     next: {
+//       value: 5,
+//       next: {
+//         value: 16,
+//         next: null,
+//       },
+//     },
+//   },
+// };
+
+class LinkedList {
+  constructor(value) {
+    this.head = { value, next: null };
+    this.tail = this.head;
+  }
+}
+
+const myLinkedList = new LinkedList(10);
