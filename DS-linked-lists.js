9c7f20d1569f05e9b8cb76c2cea69da864c7fb14
VladNCode <vldsmrfow@gmail.com>
2022-01-01 12:25:07 +0200

    started building a linked list

--- DS-linked-lists.js
+++ DS-linked-lists.js
@@ -15,7 +15,37 @@ class LinkedList {
   constructor(value) {
     this.head = { value, next: null };
     this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = {
+      value,
+      next: null,
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = {
+      value,
+      next: null,
+    };
+    const curr = this.head;
+    this.head = newNode;
+    this.head.next = curr;
+    this.length++;
+    return this;
   }
 }
 
 const myLinkedList = new LinkedList(10);
+
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.append(32);
+myLinkedList.prepend(55);
+console.log(myLinkedList);
