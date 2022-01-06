642e52893a5652354415ce022f0d089b9f1d8e09
VladNCode <vldsmrfow@gmail.com>
2022-01-06 10:13:42 +0200

    built BST

---
+++ DS-trees.js
@@ -0,0 +1,107 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  traverse(newNode = false, currentNode, value = false) {
+    if (newNode) {
+      while (currentNode) {
+        if (newNode.value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        }
+
+        if (newNode.value > currentNode.value) {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+
+    if (value) {
+      while (value !== currentNode.value) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            return 'there is no such value in a tree';
+          }
+          currentNode = currentNode.left;
+        }
+
+        if (value > currentNode.value) {
+          if (!currentNode.right) {
+            return 'there is no such value in a tree';
+          }
+          currentNode = currentNode.right;
+        }
+      }
+      return currentNode;
+    }
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    let currentNode;
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    if (newNode.value < this.root.value) {
+      currentNode = this.root.left;
+      if (!this.root.left) {
+        this.root.left = newNode;
+        return this;
+      }
+      this.traverse(newNode, currentNode);
+    }
+
+    if (newNode.value > this.root.value) {
+      currentNode = this.root.right;
+      if (!this.root.right) {
+        this.root.right = newNode;
+        return this;
+      }
+      this.traverse(newNode, currentNode);
+    }
+  }
+
+  lookup(value) {
+    let currentNode = this.root;
+    return this.traverse(false, currentNode, value);
+  }
+  //remove
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+tree.insert(150);
+console.log(tree.lookup(170));
+
+const traverse = function (node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+};
+
+console.log(JSON.stringify(traverse(tree.root)));
