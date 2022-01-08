8e84ce619c1a263aa8cd7b3f6e4c3cc81a92820d
VladNCode <vldsmrfow@gmail.com>
2022-01-08 11:46:30 +0200

    merge sort

--- ALGO-sorting.js
+++ ALGO-sorting.js
@@ -42,4 +42,68 @@ const selectionSort = function (arr) {
   return arr;
 };
 
-console.log(selectionSort(numbers));
+// console.log(selectionSort(numbers));
+
+// const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+const insertionSort = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; arr[j] < arr[j - 1]; j--) {
+      [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+    }
+  }
+  return arr;
+};
+
+// function insertionSort(array) {
+//   const length = array.length;
+// 	for (let i = 0; i < length; i++) {
+// 		if (array[i] < array[0]) {
+//       //move number to the first position
+//       array.unshift(array.splice(i,1)[0]);
+//     } else {
+//       // only sort number smaller than number on the left of it. This is the part of insertion sort that makes it fast if the array is almost sorted.
+//       if (array[i] < array[i-1]) {
+//         //find where number should go
+//         for (var j = 1; j < i; j++) {
+//           if (array[i] >= array[j-1] && array[i] < array[j]) {
+//             //move number to the right spot
+//             array.splice(j,0,array.splice(i,1)[0]);
+//           }
+//         }
+//       }
+//     }
+// 	}
+// }
+
+// console.log(insertionSort(numbers));
+
+const mergeSort = function (arr) {
+  if (arr.length === 1) return arr;
+
+  const middle = Math.floor(arr.length / 2);
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+const merge = function (left, right) {
+  const fin = [];
+  let leftInd = 0;
+  let rightInd = 0;
+
+  while (leftInd < left.length && rightInd < right.length) {
+    if (left[leftInd] < right[rightInd]) {
+      fin.push(left[leftInd]);
+      leftInd++;
+    } else {
+      fin.push(right[rightInd]);
+      rightInd++;
+    }
+  }
+
+  return [...fin, ...left.slice(leftInd), ...right.slice(rightInd)];
+};
+
+console.log(mergeSort(numbers));
