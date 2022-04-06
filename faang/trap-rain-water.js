9a8754fe2220f03a744a0be4ac90684b61f067d1
Vladyslav Nikiforov <vladncode@gmail.com>
2022-04-06 19:29:35 +0300

    refactored

--- faang/trap-rain-water.js
+++ faang/trap-rain-water.js
@@ -8,24 +8,50 @@
 
 function findWater(nums) {
   let total = 0;
-  let left = 0;
-  let right = 0;
 
   for (let i = 0; i < nums.length; i++) {
-    for (let p1 = i - 1, p2 = i + 1; p1 !== nums[0] && p2 !== nums[nums.length - 1]; p1--, p2++) {
-      if (nums[p1] > 0) left = Math.max(left, nums[p1]);
-      if (nums[p2] > 0) right = Math.max(right, nums[p2]);
+    let left = 0;
+    let right = 0;
+    let leftP = i;
+    let rightP = i;
+
+    while (leftP >= 0) {
+      left = Math.max(left, nums[leftP]);
+      leftP--;
     }
 
-    const currHeght = nums[i];
-    const currWater = Math.min(left, right) - currHeght;
-    if (currWater > 0) total += currWater;
-    left = 0;
-    right = 0;
+    while (rightP < nums.length) {
+      right = Math.max(right, nums[rightP]);
+      rightP++;
+    }
+
+    const currWater = Math.min(left, right) - nums[i];
+    if (currWater >= 0) total += currWater;
+  }
+
+  return total;
+}
+
+function findWater2(blocks) {
+  let total = 0;
+  let maxLeft = blocks[0];
+  let maxRight = blocks[blocks.length - 1];
+  let p1 = 0;
+  let p2 = blocks.length - 1;
+
+  while (p1 < p2) {
+    maxLeft <= maxRight ? p1++ : p2--; // Find out which pointer to move
+    maxLeft = Math.max(maxLeft, blocks[p1]); // Check if we need to update max left
+    total += blocks[p1] < maxLeft ? maxLeft - blocks[p1] : 0; // Check if we need to update total
+    maxRight = Math.max(maxRight, blocks[p2]); // Check if we need to update max right
+    total += blocks[p2] < maxRight ? maxRight - blocks[p2] : 0; // Check if we need to update total
   }
 
   return total;
 }
 
 const a1 = [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2];
-console.log(findWater(a1));
+const a2 = [3, 0, 0, 0, 3];
+
+console.log(findWater2(a1));
+console.log(findWater2(a2));
